5ab0b12e4dcbdcefbe8504902bf6d792ef961912
ankush <ankushshri1983@gmail.com>
2023-02-20 23:52:22 +0530

    imple file added

---
+++ src/main/java/com/example/dem/service/impl/MyfirstServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.dem.service.impl;
+
+import com.example.service.MyfirstService;
+
+public class MyfirstServiceImpl implements MyfirstService {
+
+	@Override
+	public void Test(String str) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Test2(String str1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
